cf7e0e29b5e7aff82f81f730c54b9ec58140b690
Victor Ferreira <victorcriri313@gmail.com>
2026-04-17 09:24:10 -0300

    Update ExemploModel.js

--- src/models/ExemploModel.js
+++ src/models/ExemploModel.js
@@ -1,10 +1,11 @@
 import prisma from '../lib/services/prismaClient.js';
 
 export default class ExemploModel {
+    constructor({ id = null, nome} = {}) {
     constructor({ id = null, nome, foto = null, documento = null } = {}) {
         this.id = id;
         this.nome = nome;
-        this.foto = foto;
+        this.foto = foto;           
         this.documento = documento;
     }
 
@@ -12,6 +13,8 @@ export default class ExemploModel {
         return prisma.exemplo.create({
             data: {
                 nome: this.nome,
+                foto: this.foto,
+                documento: this.documento
             },
         });
     }
@@ -19,30 +22,7 @@ export default class ExemploModel {
     async atualizar() {
         return prisma.exemplo.update({
             where: { id: this.id },
+            data: { nome: this.nome },
             data: { nome: this.nome, foto: this.foto, documento: this.documento },
         });
     }
-
-    async deletar() {
-        return prisma.exemplo.delete({ where: { id: this.id } });
-    }
-
-    static async buscarTodos(filtros = {}) {
-        const where = {};
-
-        if (filtros.nome) {
-            where.nome = { contains: filtros.nome, mode: 'insensitive' };
-        }
-        
-
-        return prisma.exemplo.findMany({ where });
-    }
-
-    static async buscarPorId(id) {
-        const data = await prisma.exemplo.findUnique({ where: { id } });
-        if (!data) {
-            return null;
-        }
-        return new ExemploModel(data);
-    }
-}
