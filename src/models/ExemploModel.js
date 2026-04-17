3b65c58a387b84e4e206c6e2d698356bd04fb87c
Victor Ferreira <victorcriri313@gmail.com>
2026-04-17 08:16:22 -0300

    fix constructor

--- src/models/ExemploModel.js
+++ src/models/ExemploModel.js
@@ -1,15 +1,19 @@
 import prisma from '../lib/services/prismaClient.js';
 
 export default class ExemploModel {
-    constructor({ id = null, nome} = {}) {
+    constructor({ id = null, nome, foto = null, documento = null } = {}) {
         this.id = id;
         this.nome = nome;
+        this.foto = foto;           
+        this.documento = documento;
     }
 
     async criar() {
         return prisma.exemplo.create({
             data: {
                 nome: this.nome,
+                foto: this.foto,
+                documento: this.documento
             },
         });
     }
@@ -17,7 +21,7 @@ export default class ExemploModel {
     async atualizar() {
         return prisma.exemplo.update({
             where: { id: this.id },
-            data: { nome: this.nome },
+            data: { nome: this.nome, foto: this.foto, documento: this.documento },
         });
     }
 
